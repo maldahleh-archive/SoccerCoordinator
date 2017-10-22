0fa13734e526814707b43f851c33edf8d145c795
Mohammed Al-Dahleh <maldahleh@gmail.com>
2017-10-22 16:52:21 -0400

    Update data structure and add necessary methods

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -1,23 +1,50 @@
 /*
-     Define all the possible players that will be assigned to teams
+     Dictionary to include all the possible players that will be assigned to teams
  */
 let players = [
-    "Joe Smith": ["height": 42, "experience": true, "guardians": "Jim and Jan Smith"],
-    "Jill Tanner": ["height": 36, "experience": true, "guardians": "Clara Tanner"],
-    "Bill Bon": ["height": 43, "experience": true, "guardians": "Sara and Jenny Bon"],
-    "Eva Gordon": ["height": 45, "experience": false, "guardians": "Wendy and Mike Gordon"],
-    "Matt Gill": ["height": 40, "experience": false, "guardians": "Charles and Sylvia Gill"],
-    "Kimmy Stein": ["height": 41, "experience": false, "guardians": "Bill and Hillary Stein"],
-    "Sammy Adams": ["height": 45, "experience": false, "guardians": "Jeff Adams"],
-    "Karl Saygan": ["height": 42, "experience": true, "guardians": "Heather Bledsoe"],
-    "Suzane Greenberg": ["height": 44, "experience": true, "guardians": "Henrietta Dumas"],
-    "Sal Dali": ["height": 41, "experience": false, "guardians": "Gala Dali"],
-    "Joe Kavalier": ["height": 39, "experience": false, "guardians": "Sam and Elaine Kavalier"],
-    "Ben Finkelstein": ["height": 44, "experience": false, "guardians": "Aaron and Jill Finkelstein"],
-    "Diego Soto": ["height": 41, "experience": true, "guardians": "Robin and Sarika Soto"],
-    "Chloe Alaska": ["height": 47, "experience": false, "guardians": "David and Jamie Alaska"],
-    "Arnold Willis": ["height": 43, "experience": false, "guardians": "Claire Willis"],
-    "Phillip Helm": ["height": 44, "experience": true, "guardians": "Thomas Helm and Eva Jones"],
-    "Les Clay": ["height": 42, "experience": true, "guardians": "Wynonna Brown"],
-    "Herschel Krustofski": ["height": 45, "experience": true, "guardians": "Hyman and Rachel Krustofski"]
+    ["name": "Joe Smith", "height": 42, "experience": true, "guardians": "Jim and Jan Smith"],
+    ["name": "Jill Tanner", "height": 36, "experience": true, "guardians": "Clara Tanner"],
+    ["name": "Bill Bon", "height": 43, "experience": true, "guardians": "Sara and Jenny Bon"],
+    ["name": "Eva Gordon", "height": 45, "experience": false, "guardians": "Wendy and Mike Gordon"],
+    ["name": "Matt Gill", "height": 40, "experience": false, "guardians": "Charles and Sylvia Gill"],
+    ["name": "Kimmy Stein", "height": 41, "experience": false, "guardians": "Bill and Hillary Stein"],
+    ["name": "Sammy Adams", "height": 45, "experience": false, "guardians": "Jeff Adams"],
+    ["name": "Karl Saygan", "height": 42, "experience": true, "guardians": "Heather Bledsoe"],
+    ["name": "Suzane Greenberg", "height": 44, "experience": true, "guardians": "Henrietta Dumas"],
+    ["name": "Sal Dali", "height": 41, "experience": false, "guardians": "Gala Dali"],
+    ["name": "Joe Kavalier", "height": 39, "experience": false, "guardians": "Sam and Elaine Kavalier"],
+    ["name": "Ben Finkelstein", "height": 44, "experience": false, "guardians": "Aaron and Jill Finkelstein"],
+    ["name": "Diego Soto", "height": 41, "experience": true, "guardians": "Robin and Sarika Soto"],
+    ["name": "Chloe Alaska", "height": 47, "experience": false, "guardians": "David and Jamie Alaska"],
+    ["name": "Arnold Willis", "height": 43, "experience": false, "guardians": "Claire Willis"],
+    ["name": "Phillip Helm", "height": 44, "experience": true, "guardians": "Thomas Helm and Eva Jones"],
+    ["name": "Les Clay", "height": 42, "experience": true, "guardians": "Wynonna Brown"],
+    ["name": "Herschel Krustofski", "height": 45, "experience": true, "guardians": "Hyman and Rachel Krustofski"]
 ]
+
+/*
+     Dictionaries to sort players into experienced and unexperienced sets
+ */
+var experiencedPlayers: [String: Any] = []
+var unexperiencedPlayers: [String, Any] = []
+
+/*
+     Dictionaries for each independent team
+ */
+let teamSharks: [String, Any] = []
+let teamDragons: [String, Any] = []
+let teamRaptors: [String, Any] = []
+
+
+/*
+     Function that when called will sort all players into either experienced or unexperienced categories
+ */
+func sortPlayers() {
+    for player in players {
+        if player["experience"] as! Bool {
+            experiencedPlayers.append(player)
+        } else {
+            unexperiencedPlayers.append(player)
+        }
+    }
+}
