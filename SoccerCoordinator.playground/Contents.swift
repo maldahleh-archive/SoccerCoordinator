68e514579484d5fdb3be11833e0b6fc24579f4a0
Mohammed Al-Dahleh <maldahleh@gmail.com>
2017-10-22 16:41:02 -0400

    Inclusion of all players

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -5,5 +5,19 @@ let players = [
     "Joe Smith": ["height": 42, "experience": true, "guardians": "Jim and Jan Smith"],
     "Jill Tanner": ["height": 36, "experience": true, "guardians": "Clara Tanner"],
     "Bill Bon": ["height": 43, "experience": true, "guardians": "Sara and Jenny Bon"],
-    "Eva Gordon": ["height": 45, "experience": false, "guardians": "Wendy and Mike Gordon"]
+    "Eva Gordon": ["height": 45, "experience": false, "guardians": "Wendy and Mike Gordon"],
+    "Matt Gill": ["height": 40, "experience": false, "guardians": "Charles and Sylvia Gill"],
+    "Kimmy Stein": ["height": 41, "experience": false, "guardians": "Bill and Hillary Stein"],
+    "Sammy Adams": ["height": 45, "experience": false, "guardians": "Jeff Adams"],
+    "Karl Saygan": ["height": 42, "experience": true, "guardians": "Heather Bledsoe"],
+    "Suzane Greenberg": ["height": 44, "experience": true, "guardians": "Henrietta Dumas"],
+    "Sal Dali": ["height": 41, "experience": false, "guardians": "Gala Dali"],
+    "Joe Kavalier": ["height": 39, "experience": false, "guardians": "Sam and Elaine Kavalier"],
+    "Ben Finkelstein": ["height": 44, "experience": false, "guardians": "Aaron and Jill Finkelstein"],
+    "Diego Soto": ["height": 41, "experience": true, "guardians": "Robin and Sarika Soto"],
+    "Chloe Alaska": ["height": 47, "experience": false, "guardians": "David and Jamie Alaska"],
+    "Arnold Willis": ["height": 43, "experience": false, "guardians": "Claire Willis"],
+    "Phillip Helm": ["height": 44, "experience": true, "guardians": "Thomas Helm and Eva Jones"],
+    "Les Clay": ["height": 42, "experience": true, "guardians": "Wynonna Brown"],
+    "Herschel Krustofski": ["height": 45, "experience": true, "guardians": "Hyman and Rachel Krustofski"]
 ]
