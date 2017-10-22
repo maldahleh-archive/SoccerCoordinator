fa6cbba3d05d111057850ad8319ec52b5bed9036
Mohammed Al-Dahleh <maldahleh@gmail.com>
2017-10-22 16:27:37 -0400

    Project Framework

---
+++ SoccerCoordinator.playground/Contents.swift
@@ -0,0 +1,9 @@
+/*
+     Define all the possible players that will be assigned to teams
+ */
+let players = [
+    "Joe Smith": ["height": 42, "experience": true, "guardians": "Jim and Jan Smith"],
+    "Jill Tanner": ["height": 36, "experience": true, "guardians": "Clara Tanner"],
+    "Bill Bon": ["height": 43, "experience": true, "guardians": "Sara and Jenny Bon"],
+    "Eva Gordon": ["height": 45, "experience": false, "guardians": "Wendy and Mike Gordon"]
+]
